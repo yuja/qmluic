8d601531d64804ca0f128e601aefa1cbb848b4ae
Yuya Nishihara <yuya@tcha.org>
2022-06-04 20:18:21 +0900

    typemap: add iterator to walk up lexical type tree

--- lib/src/typemap/core.rs
+++ lib/src/typemap/core.rs
@@ -1,6 +1,7 @@
 use super::enum_::Enum;
 use super::{ParentSpace, Type};
 use std::borrow::Cow;
+use std::iter::FusedIterator;
 
 /// Interface to look up type by name.
 pub trait TypeSpace<'a> {
@@ -64,3 +65,43 @@ pub trait TypeSpace<'a> {
     /// an arbitrary static constant.
     fn get_enum_by_variant(&self, name: &str) -> Option<Enum<'a>>;
 }
+
+/// Iterator to walk up the type tree.
+///
+/// This is basically a [`std::iter::successors()`] iterator with slightly different lifetime
+/// requirement.
+#[derive(Clone, Debug)]
+struct LexicalAncestorSpaces<'a, 'b> {
+    next_ty: Option<&'b ParentSpace<'a>>,
+}
+
+impl<'a, 'b> LexicalAncestorSpaces<'a, 'b> {
+    fn new<T>(ty: &'b T) -> Self
+    where
+        T: TypeSpace<'a> + ?Sized,
+    {
+        LexicalAncestorSpaces {
+            next_ty: ty.lexical_parent(),
+        }
+    }
+}
+
+impl<'a, 'b> Iterator for LexicalAncestorSpaces<'a, 'b> {
+    type Item = &'b ParentSpace<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let ty = self.next_ty;
+        self.next_ty = ty.and_then(|t| t.lexical_parent());
+        ty
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        if self.next_ty.is_some() {
+            (1, None)
+        } else {
+            (0, Some(0))
+        }
+    }
+}
+
+impl<'a, 'b> FusedIterator for LexicalAncestorSpaces<'a, 'b> {}
