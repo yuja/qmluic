f39093272ed18c6d8dbf154e04a767396dae6df6
Yuya Nishihara <yuya@tcha.org>
2022-06-22 10:54:09 +0900

    cmake: extract utility function to register UI qml files to project

---
+++ cmake/QmluicMacros.cmake
@@ -0,0 +1,23 @@
+cmake_minimum_required(VERSION 3.12)
+
+set(QMLUIC_COMMAND cargo run --)
+
+function(qmluic_target_qml_sources target)
+  set(qml_files ${ARGN})
+  list(TRANSFORM qml_files PREPEND "${CMAKE_CURRENT_SOURCE_DIR}/" OUTPUT_VARIABLE abs_qml_files)
+  list(TRANSFORM qml_files TOLOWER OUTPUT_VARIABLE ui_files)
+  list(TRANSFORM ui_files REPLACE "\.qml$" ".ui")
+  list(TRANSFORM ui_files PREPEND "${CMAKE_CURRENT_BINARY_DIR}/" OUTPUT_VARIABLE abs_ui_files)
+
+  target_sources(${target} PRIVATE ${qml_files})
+  add_custom_command(
+    OUTPUT ${abs_ui_files}
+    COMMAND ${QMLUIC_COMMAND} generate-ui -O "${CMAKE_CURRENT_BINARY_DIR}" -- ${qml_files}
+    DEPENDS ${abs_qml_files}
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+    COMMENT "Generating UI from QML"
+  )
+
+  qt_wrap_ui(header_files ${abs_ui_files})
+  target_sources(${target} PRIVATE ${header_files})
+endfunction()
