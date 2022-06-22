328fb2e2f8268dde1ab8ef2ed1b4fd380d558fb2
Yuya Nishihara <yuya@tcha.org>
2022-06-22 12:43:18 +0900

    cmake: add option to put generated .ui files in specified directory

--- cmake/QmluicMacros.cmake
+++ cmake/QmluicMacros.cmake
@@ -3,16 +3,22 @@ cmake_minimum_required(VERSION 3.12)
 find_program(QMLUIC_COMMAND qmluic DOC "qmluic command")
 
 function(qmluic_target_qml_sources target)
-  set(qml_files ${ARGN})
+  cmake_parse_arguments(PARSE_ARGV 1 arg "" "OUTPUT_DIRECTORY" "")
+  set(qml_files ${arg_UNPARSED_ARGUMENTS})
+  set(output_directory ${arg_OUTPUT_DIRECTORY})
+  if(NOT output_directory)
+    set(output_directory ${CMAKE_CURRENT_BINARY_DIR})
+  endif()
+
   list(TRANSFORM qml_files PREPEND "${CMAKE_CURRENT_SOURCE_DIR}/" OUTPUT_VARIABLE abs_qml_files)
   list(TRANSFORM qml_files TOLOWER OUTPUT_VARIABLE ui_files)
   list(TRANSFORM ui_files REPLACE "\.qml$" ".ui")
-  list(TRANSFORM ui_files PREPEND "${CMAKE_CURRENT_BINARY_DIR}/" OUTPUT_VARIABLE abs_ui_files)
+  list(TRANSFORM ui_files PREPEND "${output_directory}/" OUTPUT_VARIABLE abs_ui_files)
 
   target_sources(${target} PRIVATE ${qml_files})
   add_custom_command(
     OUTPUT ${abs_ui_files}
-    COMMAND ${QMLUIC_COMMAND} generate-ui -O "${CMAKE_CURRENT_BINARY_DIR}" -- ${qml_files}
+    COMMAND ${QMLUIC_COMMAND} generate-ui -O "${output_directory}" -- ${qml_files}
     DEPENDS ${abs_qml_files}
     WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
     COMMENT "Generating UI from QML"
