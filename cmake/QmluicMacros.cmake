f9a36fb33465805c5ca49013a8e319be3258a1c0
Yuya Nishihara <yuya@tcha.org>
2022-06-23 20:59:20 +0900

    cmake: add rule to generate/install qmldir and plugins.qmltypes
    This is far more complicated than the Makefile implementation, but we'll
    have to do that in CMake for better platform compatibility.

--- cmake/QmluicMacros.cmake
+++ cmake/QmluicMacros.cmake
@@ -33,3 +33,56 @@ function(qmluic_target_qml_sources target)
   qt_wrap_ui(header_files ${abs_ui_files})
   target_sources(${target} PRIVATE ${header_files})
 endfunction()
+
+# Adds custom target that creates QML type stubs.
+#
+# This is primarily designed for internal use, but you can use this to generate
+# type stubs by yourself.
+#
+# Example:
+#   qmluic_add_qmldir(qmluic.QtWidgets Qt5::Widgets) to
+#   generate imports/qmluic/QtWidgets/{qmldir,plugins.qmltypes}
+function(qmluic_add_qmldir module_name lib)
+  get_target_property(lib_location ${lib} LOCATION)
+  get_filename_component(lib_directory "${lib_location}" DIRECTORY)
+  set(metatypes_directory "${lib_directory}/metatypes")
+
+  # Look up metatypes.json by pattern matching. Maybe this can be resolved from
+  # the INTERFACE_SOURCES property with TARGET_PROPERTY:QT_CONSUMES_METATYPES on Qt 6,
+  # but doing that would be tedious.
+  string(REPLACE "::" "" metatypes_prefix ${lib})
+  string(TOLOWER "${metatypes_prefix}" metatypes_prefix)
+  file(GLOB input_metatypes_file "${metatypes_directory}/${metatypes_prefix}_*.json")
+  list(LENGTH input_metatypes_file count)
+  if(count EQUAL 0)
+    message(FATAL_ERROR "No metatypes.json found for ${lib}")
+  elseif(NOT count EQUAL 1)
+    message(FATAL_ERROR "Multiple metatypes.json found for ${lib}: ${input_metatypes_file}")
+  endif()
+
+  string(REPLACE "." "/" module_directory ${module_name})
+  set(output_directory "${CMAKE_CURRENT_BINARY_DIR}/imports/${module_directory}")
+  set(output_qmldir "${output_directory}/qmldir")
+  set(output_qmltypes "${output_directory}/plugins.qmltypes")
+
+  add_custom_command(
+    OUTPUT "${output_qmldir}"
+    COMMAND ${CMAKE_COMMAND} -E make_directory "${output_directory}"
+    COMMAND ${CMAKE_COMMAND} -E echo "module ${module_name}" > "${output_qmldir}"
+    COMMAND ${CMAKE_COMMAND} -E echo "typeinfo plugins.qmltypes" >> "${output_qmldir}"
+    COMMAND ${CMAKE_COMMAND} -E echo "depends QtQml" >> "${output_qmldir}"
+  )
+
+  get_target_property(QMAKE_EXECUTABLE Qt::qmake LOCATION)
+  add_custom_command(
+    OUTPUT "${output_qmltypes}"
+    COMMAND
+      ${CARGO_COMMAND} run -- dump-metatypes --qmake "${QMAKE_EXECUTABLE}"
+                                             --output-qmltypes "${output_qmltypes}"
+                                             "${input_metatypes_file}"
+    MAIN_DEPENDENCY "${input_metatypes_file}"
+    DEPENDS Qt::qmake qmluic
+  )
+
+  add_custom_target(module_name ALL DEPENDS "${output_qmldir}" "${output_qmltypes}")
+endfunction()
