a89f03560fbfb2da77ada43d0026aeb0cc458b4b
Yuya Nishihara <yuya@tcha.org>
2022-06-29 17:59:22 +0900

    qtname: implement variable name generator compatible with Driver::qtify()

--- lib/src/qtname.rs
+++ lib/src/qtname.rs
@@ -57,6 +57,35 @@ pub fn is_std_set_property(name: &str, write_func_name: Option<&str>) -> bool {
     }
 }
 
+/// Generates variable name candidate for the given type.
+///
+/// The `type_name` should be a valid identifier. `Scoped::Name` is invalid for example.
+///
+/// This follows the convention of `Driver::qtify()` defined in
+/// `qtbase/src/tools/uic/driver.cpp`, but does not return an empty string unless the given
+/// `type_name` is empty.
+pub fn variable_name_for_type<S>(type_name: S) -> String
+where
+    S: AsRef<str>,
+{
+    let type_name = type_name.as_ref();
+    let mut var_name = String::with_capacity(type_name.len());
+    let mut chars = type_name.chars().fuse();
+    if type_name.starts_with(|c| c == 'Q' || c == 'K')
+        && type_name[1..].starts_with(|c: char| c.is_ascii_alphabetic())
+    {
+        chars.next();
+    }
+    for c in chars.by_ref() {
+        var_name.push(c.to_ascii_lowercase());
+        if !c.is_ascii_uppercase() {
+            break;
+        }
+    }
+    var_name.extend(chars);
+    var_name
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -67,5 +96,13 @@ mod tests {
         assert!(!is_std_set_property("std", Some("updateStd")));
         assert!(!is_std_set_property("std", None));
         assert!(!is_std_set_property("", Some("set")));
+    }
+
+    #[test]
+    fn qtified_variable_name() {
+        assert_eq!(variable_name_for_type("QMainWindow"), "mainWindow");
+        assert_eq!(variable_name_for_type("QVBoxLayout"), "vboxLayout");
+        assert_eq!(variable_name_for_type("Q"), "q");
+        assert_eq!(variable_name_for_type("Q3D"), "q3D");
     }
 }
