731261c75653415f826f665c99f08bcfde928a1b
Yuya Nishihara <yuya@tcha.org>
2022-07-01 14:23:45 +0900

    cmake: add DYNAMIC_BINDING option to qmluic_target_qml_sources()

--- cmake/QmluicMacros.cmake
+++ cmake/QmluicMacros.cmake
@@ -1,30 +1,45 @@
 cmake_minimum_required(VERSION 3.12)
 
 function(qmluic_target_qml_sources target)
-  cmake_parse_arguments(PARSE_ARGV 1 arg "" "OUTPUT_DIRECTORY" "")
+  cmake_parse_arguments(PARSE_ARGV 1 arg "DYNAMIC_BINDING" "OUTPUT_DIRECTORY" "")
   set(qml_files ${arg_UNPARSED_ARGUMENTS})
+  set(dynamic_binding ${arg_DYNAMIC_BINDING})
   set(output_directory ${arg_OUTPUT_DIRECTORY})
   if(NOT output_directory)
     set(output_directory ${CMAKE_CURRENT_BINARY_DIR})
   endif()
+
+  set(generate_ui_opts)
 
   list(TRANSFORM qml_files PREPEND "${CMAKE_CURRENT_SOURCE_DIR}/" OUTPUT_VARIABLE abs_qml_files)
   list(TRANSFORM qml_files TOLOWER OUTPUT_VARIABLE ui_files)
   list(TRANSFORM ui_files REPLACE "\.qml$" ".ui")
   list(TRANSFORM ui_files PREPEND "${output_directory}/" OUTPUT_VARIABLE abs_ui_files)
 
+  if(dynamic_binding)
+    list(TRANSFORM qml_files TOLOWER OUTPUT_VARIABLE ui_support_h_files)
+    list(TRANSFORM ui_support_h_files REPLACE "([^/]*)\.qml$" "uisupport_\\1.h")
+    list(TRANSFORM ui_support_h_files PREPEND "${output_directory}/" OUTPUT_VARIABLE abs_ui_support_h_files)
+    list(APPEND generate_ui_opts "--dynamic-binding")
+  else()
+    set(ui_support_h_files)
+    set(abs_ui_support_h_files)
+  endif()
+
   target_sources(${target} PRIVATE ${qml_files})
   get_target_property(QMAKE_EXECUTABLE Qt::qmake LOCATION)
   add_custom_command(
-    OUTPUT ${abs_ui_files}
+    OUTPUT ${abs_ui_files} ${abs_ui_support_h_files}
     COMMAND
       Qmluic::qmluic generate-ui -O "${output_directory}"
                                  --qmake "${QMAKE_EXECUTABLE}"
+                                 ${generate_ui_opts}
                                  -- ${qml_files}
     DEPENDS ${abs_qml_files} Qmluic::qmluic Qt::qmake
     WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
     COMMENT "Generating UI from QML"
   )
+  target_sources(${target} PRIVATE ${abs_ui_support_h_files})
 
   qt_wrap_ui(header_files ${abs_ui_files})
   target_sources(${target} PRIVATE ${header_files})
