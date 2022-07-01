b284ef352c37a2a72800261b27e7a04707da9a2b
Yuya Nishihara <yuya@tcha.org>
2022-07-01 14:23:45 +0900

    cmake: add DYNAMIC_BINDING option to qmluic_target_qml_sources()

--- cmake/QmluicShim.cmake
+++ cmake/QmluicShim.cmake
@@ -8,8 +8,9 @@
 #   endif()
 
 function(qmluic_target_qml_sources target)
-  cmake_parse_arguments(PARSE_ARGV 1 arg "" "OUTPUT_DIRECTORY" "")
+  cmake_parse_arguments(PARSE_ARGV 1 arg "DYNAMIC_BINDING" "OUTPUT_DIRECTORY" "")
   set(qml_files ${arg_UNPARSED_ARGUMENTS})
+  set(dynamic_binding ${arg_DYNAMIC_BINDING})
   set(output_directory ${arg_OUTPUT_DIRECTORY})
   if(NOT output_directory)
     message(FATAL_ERROR "OUTPUT_DIRECTORY must be set (shim can't generate .ui from .qml)")
@@ -19,7 +20,17 @@ function(qmluic_target_qml_sources target)
   list(TRANSFORM ui_files REPLACE "\.qml$" ".ui")
   list(TRANSFORM ui_files PREPEND "${output_directory}/" OUTPUT_VARIABLE abs_ui_files)
 
+  if(dynamic_binding)
+    list(TRANSFORM qml_files TOLOWER OUTPUT_VARIABLE ui_support_h_files)
+    list(TRANSFORM ui_support_h_files REPLACE "([^/]*)\.qml$" "uisupport_\\1.h")
+    list(TRANSFORM ui_support_h_files PREPEND "${output_directory}/" OUTPUT_VARIABLE abs_ui_support_h_files)
+  else()
+    set(ui_support_h_files)
+    set(abs_ui_support_h_files)
+  endif()
+
   target_sources(${target} PRIVATE ${qml_files})
+  target_sources(${target} PRIVATE ${abs_ui_support_h_files})
 
   qt_wrap_ui(header_files ${abs_ui_files})
   target_sources(${target} PRIVATE ${header_files})
