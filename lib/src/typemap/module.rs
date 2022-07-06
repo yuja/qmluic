5b1faa0ab2a2f748f7a7359b23df090c4cc2e827
Yuya Nishihara <yuya@tcha.org>
2022-07-06 20:07:55 +0900

    typemap: report bad module imports on type lookup
    It wasn't easy to propagate module reference error from the constructor
    of ImportedModuleSpace. I also think it's better to delay the error reporting
    to the type resolution phase so uigen can run with half-baked user qmldir
    modules.

--- lib/src/typemap/module.rs
+++ lib/src/typemap/module.rs
@@ -9,7 +9,7 @@ use camino::Utf8Path;
 use std::borrow::Cow;
 
 /// Top-level module identifier.
-#[derive(Clone, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub enum ModuleId<'s> {
     /// Built-in types.
     Builtins,
@@ -19,6 +19,16 @@ pub enum ModuleId<'s> {
     ///
     /// The path must be normalized in a certain form. Typically it is an absolute path.
     Directory(Cow<'s, Utf8Path>),
+}
+
+impl<'s> From<&ModuleId<'s>> for ModuleId<'static> {
+    fn from(id: &ModuleId<'s>) -> Self {
+        match id {
+            ModuleId::Builtins => ModuleId::Builtins,
+            ModuleId::Named(name) => ModuleId::Named(name.clone().into_owned().into()),
+            ModuleId::Directory(path) => ModuleId::Directory(path.clone().into_owned().into()),
+        }
+    }
 }
 
 impl<'s> AsRef<ModuleId<'s>> for ModuleId<'s> {
@@ -109,7 +119,7 @@ impl Extend<metatype::Enum> for ModuleData {
 /// Stack of modules which represents a type space containing imported modules.
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct ImportedModuleSpace<'a> {
-    data_stack: Vec<TypeDataRef<'a, ModuleData>>,
+    data_stack: Vec<Result<TypeDataRef<'a, ModuleData>, ModuleId<'static>>>,
     type_map: TypeMapRef<'a>,
 }
 
@@ -126,11 +136,16 @@ impl<'a> ImportedModuleSpace<'a> {
         I: IntoIterator,
         I::Item: AsRef<ModuleId<'s>>,
     {
-        // TODO: module not found error?
         let data_stack = modules
             .into_iter()
-            .filter_map(|id| type_map.as_ref().get_module_data(id))
-            .map(TypeDataRef)
+            .map(|id| {
+                let id = id.as_ref();
+                type_map
+                    .as_ref()
+                    .get_module_data(id)
+                    .ok_or_else(|| id.into())
+                    .map(TypeDataRef)
+            })
             .collect();
         ImportedModuleSpace {
             data_stack,
@@ -145,7 +160,7 @@ impl<'a> ImportedModuleSpace<'a> {
         S: AsRef<ModuleId<'s>>,
     {
         if let Some(d) = self.type_map.as_ref().get_module_data(id) {
-            self.data_stack.push(TypeDataRef(d));
+            self.data_stack.push(Ok(TypeDataRef(d)));
             true
         } else {
             false
@@ -160,10 +175,11 @@ impl<'a> TypeSpace<'a> for ImportedModuleSpace<'a> {
     }
 
     fn get_type(&self, name: &str) -> Option<Result<NamedType<'a>, TypeMapError>> {
-        self.data_stack.iter().rev().find_map(|d| {
-            d.as_ref().namespace.get_type_with(name, self.type_map, || {
+        self.data_stack.iter().rev().find_map(|r| match r {
+            Ok(d) => d.as_ref().namespace.get_type_with(name, self.type_map, || {
                 ParentSpace::Namespace(d.as_ref().to_namespace(self.type_map))
-            })
+            }),
+            Err(id) => Some(Err(TypeMapError::InvalidModuleRef(id.clone()))),
         })
     }
 
